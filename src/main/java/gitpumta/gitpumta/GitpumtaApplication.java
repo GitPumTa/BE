4570975215be38a03d873da45ae2443442969bd5
JEKKY <137982621+vJEKKYv@users.noreply.github.com>
2025-07-09 13:28:30 +0900

    init: spring 기본 파일

---
+++ src/main/java/gitpumta/gitpumta/GitpumtaApplication.java
@@ -0,0 +1,13 @@
+package gitpumta.gitpumta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpumtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpumtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gitpumta/gitpumta/GitpumtaApplicationTests.java
@@ -0,0 +1,13 @@
+package gitpumta.gitpumta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpumtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
