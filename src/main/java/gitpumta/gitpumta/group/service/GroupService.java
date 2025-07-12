aaa1a0c83a64f5cc04afc441b3e960f550e376d3
zeew00kim <tukzeew00@gmail.com>
2025-07-12 17:23:55 +0900

    feat: 그그룹 생성 기능 구현 및 저장 처리

--- src/main/java/gitpumta/gitpumta/group/service/GroupService.java
+++ src/main/java/gitpumta/gitpumta/group/service/GroupService.java
@@ -1,22 +1,29 @@
 package gitpumta.gitpumta.group.service;
 
-
 import gitpumta.gitpumta.group.bean.CreateGroupBean;
 import gitpumta.gitpumta.group.domain.GroupDAO;
 import gitpumta.gitpumta.group.domain.dto.CreateGroupRequestDTO;
-import gitpumta.gitpumta.group.domain.dto.GroupResponseDTO;
+import gitpumta.gitpumta.group.repository.GroupDAORepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 public class GroupService {
-    private CreateGroupBean createGroupBean;
-    public GroupService(CreateGroupBean createGroupBean) {
+
+    private final CreateGroupBean createGroupBean;
+    private final GroupDAORepository groupRepository;
+
+    public GroupService(CreateGroupBean createGroupBean, GroupDAORepository groupRepository) {
         this.createGroupBean = createGroupBean;
+        this.groupRepository = groupRepository;
     }
+
     public UUID createGroup(CreateGroupRequestDTO createGroupRequestDTO) {
         GroupDAO groupDAO = createGroupBean.exec(createGroupRequestDTO);
-        return  groupDAO.getId();
+        groupDAO.setCreatedAt(LocalDateTime.now());
+        groupRepository.save(groupDAO);
+        return groupDAO.getId();
     }
 }
