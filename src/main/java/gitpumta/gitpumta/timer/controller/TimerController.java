3ad1d623e219f2af675431fca29697b38f10d292
ChoiGem <pk726510@gmail.com>
2025-07-14 00:06:08 +0900

    feat: 타이머 시작,정지 서비스 작서

--- src/main/java/gitpumta/gitpumta/timer/controller/TimerController.java
+++ src/main/java/gitpumta/gitpumta/timer/controller/TimerController.java
@@ -1,12 +1,49 @@
 package gitpumta.gitpumta.timer.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gitpumta.gitpumta.timer.domain.dto.StartTimerRequestDTO;
+import gitpumta.gitpumta.timer.domain.dto.StopTimerRequestDTO;
+import gitpumta.gitpumta.timer.service.TimerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/timer")
+@RequestMapping(value = "/Main/timer")
 @CrossOrigin
 public class TimerController {
-    
+    private final TimerService timerService;
+
+    public TimerController(TimerService timerService) {
+        this.timerService = timerService;
+    }
+
+    @PostMapping(value = "/start")
+    public ResponseEntity<Map<String, Object>> startTimer(@RequestBody StartTimerRequestDTO startTimerRequestDTO) {
+        timerService.StartTimer(startTimerRequestDTO);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("message", "시작 저장 성공");
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+    }
+
+    @PostMapping(value = "/stop")
+    public ResponseEntity<Map<String, Object>> stopTimer(@RequestBody StopTimerRequestDTO stopTimerRequestDTO) {
+        try {
+            timerService.StopTimer(stopTimerRequestDTO);
+            Map<String, Object> responseMap = new HashMap<>();
+            responseMap.put("message", stopTimerRequestDTO.getStatus().equals("0") ? "타이머 일시정지 성공" : "타이머 완전정지 성공");
+
+            return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+        } catch (Exception e) {
+            Map<String, Object> responseMap = new HashMap<>();
+            responseMap.put("message", e.getMessage());
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseMap);
+        }
+
+    }
 }
