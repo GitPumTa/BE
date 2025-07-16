ab472aea5a86eff32af3e398a8a17277c80ac9ff
Eunsseob <163013769+Eunsseob@users.noreply.github.com>
2025-07-16 23:01:43 +0900

    feat : 유저 전체랭킹을 위해 서비스 컨트롤 추가

--- src/main/java/gitpumta/gitpumta/user/controller/UserController.java
+++ src/main/java/gitpumta/gitpumta/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package gitpumta.gitpumta.user.controller;
 import java.util.*;
 
-import gitpumta.gitpumta.user.domain.dto.GetUserRequestDTO;
+import ch.qos.logback.classic.Logger;
 import gitpumta.gitpumta.user.domain.dto.GetUserResponseDTO;
 import gitpumta.gitpumta.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/user")
 @CrossOrigin
@@ -25,14 +27,22 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(requestMap);
     }
 
-    @GetMapping(value = "/get")
-    public ResponseEntity<Map<String, Object>> getUser(@RequestBody GetUserRequestDTO getUserRequestDTO) {
-        GetUserResponseDTO getUserResponseDTO = userService.getUser(getUserRequestDTO);
-        Map<String, Object> requestMap = new HashMap<>();
-        requestMap.put("is_success", getUserResponseDTO != null);
-        requestMap.put("message", getUserResponseDTO != null ? "유저 정보 반환 성공. " : "유저 정보 반환 실패. ");
-        requestMap.put("user", getUserResponseDTO);
+//    @GetMapping(value = "/get")
+//    public ResponseEntity<Map<String, Object>> getUser(@RequestBody GetUserRequestDTO getUserRequestDTO) {
+//        GetUserResponseDTO getUserResponseDTO = userService.getUser(getUserRequestDTO);
+//        Map<String, Object> requestMap = new HashMap<>();
+//        requestMap.put("is_success", getUserResponseDTO != null);
+//        requestMap.put("message", getUserResponseDTO != null ? "유저 정보 반환 성공. " : "유저 정보 반환 실패. ");
+//        requestMap.put("user", getUserResponseDTO);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(requestMap);
+//    }
 
-        return ResponseEntity.status(HttpStatus.OK).body(requestMap);
+    // 조회
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<GetUserResponseDTO> getUser(@PathVariable UUID id) {
+        log.info("조회 요청 UUID: {}", id);
+        GetUserResponseDTO response = userService.getUser(id);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/gitpumta/gitpumta/user/service/UserService.java
+++ src/main/java/gitpumta/gitpumta/user/service/UserService.java
@@ -1,24 +1,46 @@
 package gitpumta.gitpumta.user.service;
 
+import gitpumta.gitpumta.planner.domain.dto.PlannerResponseDTO;
 import gitpumta.gitpumta.user.bean.GetuserBean;
 import gitpumta.gitpumta.user.domain.UserDAO;
 import gitpumta.gitpumta.user.domain.dto.GetUserRequestDTO;
 import gitpumta.gitpumta.user.domain.dto.GetUserResponseDTO;
+import gitpumta.gitpumta.user.domain.dto.RankingResponseDTO;
+import gitpumta.gitpumta.user.domain.dto.UserRatingInfoDTO;
 import gitpumta.gitpumta.user.repository.UserDAORepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserDAORepository userDAORepository;
     private final GetuserBean getuserBean;
 
-    public UserService(UserDAORepository userDAORepository , GetuserBean getuserBean) {
-        this.userDAORepository = userDAORepository;
-        this.getuserBean = getuserBean;
-    }
+//    public UserService(UserDAORepository userDAORepository , GetuserBean getuserBean) {
+//        this.userDAORepository = userDAORepository;
+//        this.getuserBean = getuserBean;
+//    }
 
-    public GetUserResponseDTO getUser(GetUserRequestDTO getUserRequestDTO) {
-        return getuserBean.exec(getUserRequestDTO);
-    }
+    public GetUserResponseDTO getUser(UUID id) {
+        UserDAO user = userDAORepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
+        return GetUserResponseDTO.builder()
+                .nickname(user.getNickname())
+                .gitId(user.getGitId())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+//    // 조회 메서드
+//    public List<UUID> getUserId(UUID userId) {
+//        List<UserDAO> userDAOS = userDAORepository.findByuserIdAndDeletedAtIsNull(userId);
+//
+//        return userId.stream()
+//                .map(userId -> userId.getUser().getId())  // UserDAO → UUID
+//                .collect(Collectors.toList());
+//    }
 }
