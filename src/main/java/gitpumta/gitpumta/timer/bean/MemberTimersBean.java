f0138d0342c8d463aa474dcdd9ab3fd51c7746d5
ChoiGem <pk726510@gmail.com>
2025-07-16 13:35:42 +0900

    fix: UUID로 다시 수정

--- src/main/java/gitpumta/gitpumta/timer/bean/MemberTimersBean.java
+++ src/main/java/gitpumta/gitpumta/timer/bean/MemberTimersBean.java
@@ -1,8 +1,10 @@
 package gitpumta.gitpumta.timer.bean;
 
+import gitpumta.gitpumta.group.domain.GroupDAO;
 import gitpumta.gitpumta.group.repository.GroupDAORepository;
 import gitpumta.gitpumta.timer.domain.dto.GetMemberTimersResponseDTO;
 import gitpumta.gitpumta.timer.repository.TimerDAORepository;
+import gitpumta.gitpumta.user.domain.UserDAO;
 import gitpumta.gitpumta.user.repository.UserDAORepository;
 import org.springframework.stereotype.Component;
 
@@ -25,33 +27,47 @@ public class MemberTimersBean {
     //public GetMemberTimersResponseDTO exec(GetMemberTimersRequestDTO getMemberTimersRequestDTO) {
     //}
 
-    // 그룹 이름 가져오기 -> 그룹 테이블 검색
-    public String getMyMonitoringGroup(UUID accountId, UUID groupId) {
-        return null;
+    public String getMyMonitoringGroup(UUID groupId) {
+        GroupDAO group = groupDAORepository.findByIdAndDeletedAtIsNull(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 그룹입니다."));
+
+        return group.getName();
     }
 
     // 그룹 설명 가져오기 -> 그룹 테이블 검색
-    public String getMonitoringGroupDescription(UUID accountId, UUID groupId) {
-        return null;
+    public String getMonitoringGroupDescription(UUID groupId) {
+        GroupDAO group = groupDAORepository.findByIdAndDeletedAtIsNull(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 그룹입니다."));
+
+        return group.getDescription();
     }
 
     // 그룹 내 나의 랭킹 계산 -> 그룹 테이블 + 타이머 테이블 조인
     public int getMyRank(UUID accountId, UUID groupId) {
+
+
         return 0;
     }
 
     // 내 이름 가져오기 -> 유저 테이블 참조
     public String getMyName(UUID accountId) {
-        return null;
+        UserDAO user = userDAORepository.findById(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저 아이디입니다."));
+
+        return user.getNickname();
     }
 
     // 그룹 내 공부 시간 랭킹 리스트 -> 그룹 테이블 + 타이머 테이블 조인
     public List<GetMemberTimersResponseDTO.DurationLeaderDTO> getDurationLeaders(UUID accountId, UUID groupId) {
+
+
         return null;
     }
 
     // 그룹 내 커밋 개수 랭킹 리스트 -> 그룹 테이블 + 커밋 테이블 조인
     public List<GetMemberTimersResponseDTO.CommitLeaderDTO> getCommitLeaders(UUID accountId, UUID groupId) {
+
+
         return null;
     }
 }
